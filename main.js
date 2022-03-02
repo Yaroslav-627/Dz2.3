cb95d206e3bb318e9f9b348a51babfd19a4cb746
Yaroslav-627 <75137371+Yaroslav-627@users.noreply.github.com>
2022-03-02 11:46:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+console.log(typeof(result = 5+5+'5'));
+
+let My = 'My';
+let Name = 'name';
+let is = 'is';
+let Viktor = 'Viktor';
+fullName = My +' '+ Name +' '+ is +' '+ Viktor;
+console.log(fullName);
+
+let userName = "Олександро";
+let payment = 300;
+alert("«Дякуємо, " + userName +"! До сплати " + payment + " гривень»");
+
+let email = "Hello@gmail.com";
+if(email.includes("@")){
+    console.log("@ - Есть");
+}else{
+    console.log("@ - Нету");
+}
+console.log("Email Lenght:" + email.length);
+
+let k1 = "5px";
+let k2 = "12djd334";
+let k3 = "14.45asdwe12";
+let k4 = "qwqweeewq";
