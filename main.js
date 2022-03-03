33f8ce053c499436a9dbdb0fe54ab0a9f979c457
Yaroslav-627 <75137371+Yaroslav-627@users.noreply.github.com>
2022-03-03 17:17:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -26,3 +26,7 @@ let k1 = "5px";
 let k2 = "12djd334";
 let k3 = "14.45asdwe12";
 let k4 = "qwqweeewq";
+console.log(Number.parseInt(k1));
+console.log(Number.parseInt(k2));
+console.log(Number.parseInt(k3));
+console.log(Number.parseInt(k4));
